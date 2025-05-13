40bb2072ae3b564f8441804b702ba4aa3b614538
Angelo Gabrielli 'tintiz00rd' Tinti <angelo.tinti@gmail.com>
2025-05-13 11:38:30 -0300

    Adding TailwindCSS.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['src/__tests__/setup.ts'],
   },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 });
